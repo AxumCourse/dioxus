688e2e4c6490c022ed2cff4cd1d7df9e7d59cae8
axumrs <rolson867@gmail.com>
2025-04-28 15:49:18 +0800

    计数器：use_signal()修改状态

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -19,12 +19,13 @@ fn App() -> Element {
 
 #[component]
 fn CalcView() -> Element {
-    let mut count = use_hook(|| 0);
-    let plus_handler = move |_| count = count + 1;
+    let mut count = use_signal(|| 0);
+    let plus_handler = move |_| count.set(count + 1);
+    let sub_handler = move |_| count.set(count - 1);
     rsx! {
         div { "当前计数：{count}" }
         div {
-            button { "-1" }
+            button { onclick: sub_handler, "-1" }
             button { onclick: plus_handler, "+1" }
         }
     }
