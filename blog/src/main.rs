71666da3b72305b6a3fbfad50302180a925d8854
axumrs <rolson867@gmail.com>
2025-04-28 20:08:07 +0800

    博客：获取数据

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,4 +1,6 @@
-use dioxus::{logger::tracing::info, prelude::*};
+use std::rc::Rc;
+
+use dioxus::prelude::*;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
@@ -17,7 +19,7 @@ fn App() -> Element {
     }
 }
 
-#[derive(serde::Deserialize, Debug, Default)]
+#[derive(serde::Deserialize, Debug)]
 pub struct Post {
     #[serde(rename = "userId")]
     pub user_id: u32,
@@ -25,9 +27,10 @@ pub struct Post {
     pub title: String,
     pub body: String,
 }
+
 #[component]
 fn BlogPost() -> Element {
-    let mut post_title = use_signal(|| "".to_string());
+    let mut post = use_signal(|| None::<Rc<Post>>);
 
     let fetch_post = move |_| async move {
         let resp = reqwest::get("https://jsonplaceholder.typicode.com/posts/1")
@@ -36,12 +39,21 @@ fn BlogPost() -> Element {
             .json::<Post>()
             .await
             .unwrap();
-        info!("Post: {:#?}", resp);
-        post_title.set(resp.title);
+        post.set(Some(Rc::new(resp)));
     };
 
-    rsx! {
-        h1 { "{post_title}" }
-        button { class: "px-3 py-1 bg-gray-700 text-white", onclick: fetch_post, "获取数据" }
+    if let Some(post) = post.cloned() {
+        rsx! {
+            div { "ID: {post.id}" }
+            div { "标题：{post.title}" }
+        }
+    } else {
+        rsx! {
+            button {
+                class: "px-3 py-1 bg-gray-600 text-white",
+                onclick: fetch_post,
+                "获取数据"
+            }
+        }
     }
 }
