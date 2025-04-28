9c6925f303e9c0eae3857c0f5330ba49ec6686d3
axumrs <rolson867@gmail.com>
2025-04-28 19:49:48 +0800

    博客：获取数据

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,4 +1,4 @@
-use dioxus::prelude::*;
+use dioxus::{logger::tracing::info, prelude::*};
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
@@ -7,49 +7,41 @@ fn main() {
     dioxus::launch(App);
 }
 
-#[derive(Routable, Clone)]
-enum Route {
-    #[route("/")]
-    Home {},
-    #[route("/about")]
-    About {},
-    #[route("/detail/:id")]
-    Detail { id: i32 },
-}
-
 #[component]
 fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
 
-        Router::<Route> {}
+        BlogPost {}
     }
 }
 
-#[component]
-fn Home() -> Element {
-    rsx! {
-        div { class: "text-red-600 text-xl", "首页" }
-    }
+#[derive(serde::Deserialize, Debug, Default)]
+pub struct Post {
+    #[serde(rename = "userId")]
+    pub user_id: u32,
+    pub id: u32,
+    pub title: String,
+    pub body: String,
 }
-
 #[component]
-fn About() -> Element {
-    rsx! {
-        div { class: "text-blue-600 text-xl", "关于" }
-    }
-}
+fn BlogPost() -> Element {
+    let mut post_title = use_signal(|| "".to_string());
 
-#[component]
-fn Detail(id: i32) -> Element {
-    rsx! {
-        div { class: "text-cyan-600 text-xl", "#{id} 的详情" }
+    let fetch_post = move |_| async move {
+        let resp = reqwest::get("https://jsonplaceholder.typicode.com/posts/1")
+            .await
+            .unwrap()
+            .json::<Post>()
+            .await
+            .unwrap();
+        info!("Post: {:#?}", resp);
+        post_title.set(resp.title);
+    };
 
-        ul {
-            li {
-                Link { to: Route::Home {}, "首页" }
-            }
-        }
+    rsx! {
+        h1 { "{post_title}" }
+        button { class: "px-3 py-1 bg-gray-700 text-white", onclick: fetch_post, "获取数据" }
     }
 }
