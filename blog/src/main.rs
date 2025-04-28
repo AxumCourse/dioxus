abbd3d7999de1f27012bb025ddf464aece4a4dc6
axumrs <rolson867@gmail.com>
2025-04-28 19:12:42 +0800

    博客：简单路由

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -7,11 +7,34 @@ fn main() {
     dioxus::launch(App);
 }
 
+#[derive(Routable, Clone)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/about")]
+    About {},
+}
+
 #[component]
 fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        div { class:"text-red-600 text-xl",  "Hello, 世界" }
+
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    rsx! {
+        div { class: "text-red-600 text-xl", "首页" }
+    }
+}
+
+#[component]
+fn About() -> Element {
+    rsx! {
+        div { class: "text-blue-600 text-xl", "关于" }
     }
 }
