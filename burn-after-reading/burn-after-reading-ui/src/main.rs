eb5dd6935dc165ab355e982c387761543e59ef5c
axumrs <rolson867@gmail.com>
2025-04-29 12:31:40 +0800

    受控表单

--- burn-after-reading/burn-after-reading-ui/src/main.rs
+++ burn-after-reading/burn-after-reading-ui/src/main.rs
@@ -1,4 +1,4 @@
-use dioxus::prelude::*;
+use dioxus::{html::div, prelude::*};
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
@@ -7,70 +7,26 @@ fn main() {
     dioxus::launch(App);
 }
 
-#[derive(Routable, Clone)]
-#[rustfmt::skip]
-enum Route {
-    #[nest("/blog")]
-        #[layout(BlogLayout)]
-            #[route("/")]
-            BlogList {},
-            #[route("/:id")]
-            BlogDetail { id: u32 },
-        #[end_layout]
-    #[end_nest]
-
-    #[route("/")]
-    Home {},
-}
-
 #[component]
 fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
 
-        Router::<Route> {}
+        Form {}
     }
 }
 
 #[component]
-fn Home() -> Element {
+fn Form() -> Element {
+    let mut name = use_signal(|| "".to_string());
+
     rsx!(
-        div { "home" }
+        input { value: "{name}", oninput: move |e| name.set(e.value()) }
+        if name.read().is_empty() {
+            div { "请输入你的名字" }
+        } else {
+            div { "你的名字是：{name}" }
+        }
     )
-}
-
-#[component]
-fn BlogList() -> Element {
-    rsx! {
-        ol {
-            for i in 0..10 {
-                li {
-                    Link { to: Route::BlogDetail { id: i }, "blog #{i}" }
-                }
-            }
-        }
-    }
-}
-#[component]
-fn BlogDetail(id: u32) -> Element {
-    rsx! {
-        div { "blog detail #{id}" }
-    }
-}
-
-#[component]
-fn BlogLayout() -> Element {
-    rsx! {
-        h1 { "Blog" }
-        ul {
-            li {
-                Link { to: Route::Home {}, "首页" }
-            }
-            li {
-                Link { to: Route::BlogList {}, "博客列表" }
-            }
-        }
-        Outlet::<Route> {}
-    }
 }
